50c31e851a92ec5de9a88e122991759adba37f82
Ragini Devi <dragini1997@gmail.com>
2019-07-10 01:18:38 -0700

    Updates

---
+++ precache-manifest.937e39c2272a194963dec506ab1af50f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f75a098d71826c885c0e48eaed912a0",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "52b8f5d6d29a540f46b2",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "52b8f5d6d29a540f46b2",
+    "url": "/contactmanager/static/js/2.71ddfead.chunk.js"
+  },
+  {
+    "revision": "5606e62568522e8f7682",
+    "url": "/contactmanager/static/js/main.02f35b0c.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
